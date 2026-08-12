3f6f2c95b556be379fd43ac6288cb24e0e116ae6
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2026-08-12 15:01:12 +0100

    Add a simple reading with fixed data

--- src/DrillingTelemetry.DeviceSimulator/Program.cs
+++ src/DrillingTelemetry.DeviceSimulator/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DrillingTelemetry.DeviceSimulator;
 
-Console.WriteLine("Hello, World!");
+var reading = new TelemetryReading()
+{
+    DeviceId = "DRILL-001",
+    PressurePsi = 8250,
+    TemperatureCelsius = 117.5,
+    TimestampUtc = DateTimeOffset.UtcNow
+};
+
+Console.WriteLine("Telemetry reading generated:");
+Console.WriteLine($"Device: {reading.DeviceId}");
+Console.WriteLine($"Pressure: {reading.PressurePsi} psi");
+Console.WriteLine($"Temperature: {reading.TemperatureCelsius} °C");
+Console.WriteLine($"Timestamp: {reading.TimestampUtc:O}");
